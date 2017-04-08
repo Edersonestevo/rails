8fc8b8437424bc8c1d53a97900f3503ec0452a2a
edersonestevo@hotmail.com <edersonestevo@hotmail.com>
2017-04-08 16:04:08 -0300

    Adicionado CRUD de paginas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :pages
   devise_for :admins
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -6,7 +7,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
  root 'pages#homepage'
 
- get '/:id', to: 'pages#paginas', as: :pages
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
